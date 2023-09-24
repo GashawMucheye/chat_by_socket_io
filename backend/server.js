f6cd65ca2ee6635cbe2898d9ee39906b3817b279
gashaw mucheye <gashaw2533@gmail.com>
2023-09-24 03:04:17 +0200

    install chakra-ui and install socket.io-client

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,31 @@
+const express = require('express');
+const colors = require('colors');
+const app = express();
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 
+const PORT = process.env.PORT || 8080;
+
+const cors = require('cors');
+const { Socket } = require('dgram');
+const { log } = require('console');
+app.use(cors());
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`User Connected:${socket.id}`);
+  socket.on('send_message', (data) => {
+    console.log(data);
+  });
+});
+
+server.listen(PORT, () =>
+  console.log(`server running on port:${PORT}`.yellow.underline.bold)
+);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { Box } from '@chakra-ui/react';
+import { io } from 'socket.io-client';
+const socket = io.connect('http://localhost:8080');
+export default function App() {
+  const sendMessage = () => {
+    socket.emit('send_message', { message: 'hello' });
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box bg="darkorange">
+      <input type="text" placeholder="Enter Your Message..." />
+      <button onClick={sendMessage}>Send Message</button>
+    </Box>
+  );
 }
-
-export default App
